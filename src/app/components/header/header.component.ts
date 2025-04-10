dda544b0dccba18255f01b50020c0d0de6774771
mzkmnk <sma0729mex@gmail.com>
2025-04-10 22:30:37 +0900

    feat: create header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {NavItemComponent} from './nav-item.component';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import { tablerSearch } from '@ng-icons/tabler-icons';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NavItemComponent,
+    NgIcon,
+  ],
+  providers: [
+    provideIcons({tablerSearch})
+  ],
+  template: `
+    <header class="h-20 bg-gray-50">
+      <div class="flex h-full items-center justify-between px-6">
+        <h3 class="font-semibold">FRONTEND</h3>
+        <ul class="flex gap-10 items-center">
+          <app-nav-item item="Home" (onClickNavItem)="clickNavItem('home')" />
+          <app-nav-item item="Articles" (onClickNavItem)="clickNavItem('articles')" />
+          <app-nav-item item="About me" (onClickNavItem)="clickNavItem('aboutMe')" />
+          <ng-icon class="cursor-pointer" name="tablerSearch" />
+        </ul>
+      </div>
+    </header>
+  `
+})
+export class HeaderComponent {
+
+  clickNavItem(item: 'home'|'articles'|'aboutMe'): void {
+    console.log(`Clicked on ${item}`);
+  }
+}
