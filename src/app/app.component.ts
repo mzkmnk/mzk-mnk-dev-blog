9eabe26a0d8b681ccbb8fd254a5de6fac2353c11
mizuki <sma0729mex@gmail.com>
2025-04-11 08:21:27 +0900

    feat: header (#2)
    * refactor: templatesUrl to template
    
    * feat: add tailwind css
    
    * feat: create header component
    
    * chore: add `angular-tabler-icons` package
    
    * Revert "chore: add `angular-tabler-icons` package"
    
    This reverts commit ca3312ff8e31a0d7183b10571dd4abaf5715eea0.
    
    * chore: add `@ng-icons/tabler-icons` package
    
    * chore: add `@ng-icons/core` package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header/>
+    <router-outlet />
+  `
 })
 export class AppComponent {
-  title = 'mzk-mnk-dev-blog';
 }
---
+++ src/app/components/header/nav-item.component.ts
@@ -0,0 +1,19 @@
+import {Component, input, output} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-item',
+  template: `
+    <li class="hover:underline underline-offset-1 cursor-pointer" (click)="clickNavItem()">
+      {{item()}}
+    </li>
+  `
+})
+export class NavItemComponent {
+  item = input.required<string>();
+
+  onClickNavItem = output<void>();
+
+  clickNavItem():void {
+    this.onClickNavItem.emit();
+  }
+}
