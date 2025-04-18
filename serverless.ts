78bbe74383bd7699059e3e779a2d0492f51410fc
mzkmnk <sma0729mex@gmail.com>
2025-04-18 12:55:53 +0900

    feat: create serverless config

---
+++ serverless.ts
@@ -0,0 +1,60 @@
+import type { AWS } from '@serverless/typescript';
+
+const system = 'mzk-mnk-dev-blog';
+
+const serverlessConfig: AWS = {
+	service: system,
+
+	frameworkVersion: '3',
+
+	plugins: ['serverless-deployment-bucket'],
+
+	provider: {
+		name: 'aws',
+		stage: 'prod',
+		region: 'ap-northeast-1',
+		runtime: 'nodejs22.x',
+		architecture: 'arm64',
+		stackName: '${param:prefix}-stack',
+		stackTags: {
+			System: system,
+			Stage: '${sls:stage}',
+			Serverless: 'true',
+		},
+		deploymentBucket: {
+			name: '${param:prefix}-deployment-bucket',
+			serverSideEncryption: 'AES256',
+		},
+		iam: {
+			role: {
+				name: '${param:prefix}-lambda-role',
+			},
+		},
+		environment: {},
+	},
+
+	functions: {
+		app: {
+			handler: `dist/${system}/server/server.main`,
+			name: '${param:prefix}-lambda',
+			memorySize: 1769,
+			timeout: 60,
+			url: true,
+			package: {
+				patterns: ['!**', 'dist/**'],
+			},
+		},
+	},
+
+	package: { individually: true },
+
+	params: {
+		default: {
+			prefix: system,
+		},
+	},
+
+	resources: {},
+};
+
+module.exports = serverlessConfig;
